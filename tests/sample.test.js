c7e2f4630522054745c32b51db695d14ddc455a7
madusug <jamesmadusha@gmail.com>
2025-03-03 19:49:32 -0500

    chai-http error fix

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,18 +1,17 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
-const app = require("../src/server"); // Import Express app
+const server = require("../server"); // Ensure this path is correct
 
+// Use chai-http
 chai.use(chaiHttp);
 const { expect } = chai;
 
 describe("API Tests", function () {
   it("should return 200 for the root route", function (done) {
-    chai
-      .request(app)
+    chai.request(server)
       .get("/")
       .end((err, res) => {
         expect(res).to.have.status(200);
-        expect(res.text).to.equal("Hello, World!");
         done();
       });
   });
